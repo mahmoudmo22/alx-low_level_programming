f0351ddab012ed1d19fa6ab2a543aad37f904ead
Mahmoud Mohamed <mahmoud.soliman00@eng-st.cu.edu.eg>
2023-09-22 18:10:33 +0300

    solved a bug in fi;e 1-

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,22 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	size_t len1, len2;
-	char *new_str;
+	char *new_str, *new;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 	new_str = malloc(len1 + len2 + 1);
 	if (new_str == NULL)
 		return (NULL);
+	new = new_str;
 	while (*s1)
 	{
 		*new_str = *s1;
@@ -36,6 +41,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n--;
 	}
 	*new_str = '\0';
-	return (new_str);
+	return (new);
 }
-
