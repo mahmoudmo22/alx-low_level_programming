d64599a6e6bf62556d888e5c6eb307489bfd37e2
Mahmoud Mohamed <mahmoud.soliman00@eng-st.cu.edu.eg>
2023-09-26 20:06:03 +0300

    late solution to some of prblem of recursion problemset

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+#define ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+
+#endif
